e3c6868eafcd7bd85bbad05bb85848312c822a6d
sebas030903 <rodrigo.gutierrez.v@tecsup.edu.pe>
2025-06-09 11:27:47 -0500

    MainActivity

--- app/src/main/java/com/example/lab12/MainActivity.kt
+++ app/src/main/java/com/example/lab12/MainActivity.kt
@@ -16,32 +16,8 @@ import com.example.lab12.ui.theme.Lab12Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Lab12Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MapScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab12Theme {
-        Greeting("Android")
     }
 }
