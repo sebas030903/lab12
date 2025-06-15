36279366e537442d0d2f120924b2877069889fff
sebas030903 <rodrigo.gutierrez.v@tecsup.edu.pe>
2025-06-15 11:05:48 -0500

    MapScreen.kt modificaciones

--- app/src/main/java/com/example/lab12/MapScreen.kt
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -2,34 +2,40 @@ package com.example.lab12
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.Marker
-
+import com.example.lab12.R
 
 @Composable
 fun MapScreen() {
-    val ArequipaLocation = LatLng(-16.4040102, -71.559611) // Arequipa, Perú
+    val context = LocalContext.current
+    val arequipaLocation = LatLng(-16.4040102, -71.559611)
     val cameraPositionState = rememberCameraPositionState {
-        position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
+        position = CameraPosition.fromLatLngZoom(arequipaLocation, 12f)
     }
 
-
     Box(modifier = Modifier.fillMaxSize()) {
-        // Añadir GoogleMap al layout
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Añadir marcador en Denver, Colorado
+            // Aquí dentro ya es seguro usar BitmapDescriptorFactory
+            val customIcon: BitmapDescriptor = remember {
+                BitmapDescriptorFactory.fromResource(R.drawable.montana)
+            }
+
             Marker(
-                state = rememberMarkerState(position = ArequipaLocation),
+                state = rememberMarkerState(position = arequipaLocation),
+                icon = customIcon,
                 title = "Arequipa, Perú"
             )
         }
